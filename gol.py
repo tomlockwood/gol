29637ad3e1b776522c057ed70e9ef7630ab202bd
Tom Lockwood <tom.lockwood@bugcrowd.com>
2019-01-02 14:27:59 +1100

    Adding tests and gitignore

--- gol.py
+++ gol.py
@@ -1,16 +1,16 @@
 import random
 
 class Rule:
-  def __init__(self,defintion={}):
+  def __init__(self,definition={}):
     self.definition = definition
     self.alive = definition.get('alive')
     if self.alive == None:
-      self.alive = random.choice([True,False],1)
+      self.alive = random.choice([True,False])
 
     self.transitions = []
     if definition.get('transitions') == None:
-      for x in definition['rule_amount']:
-        self.transitions.append(random.choice(definition['rule_amount']))
+      for x in range(definition['rule_amount']):
+        self.transitions.append(random.choice(range(definition['rule_amount'])))
     else:
       self.transitions = definition['transitions']
       
@@ -20,30 +20,38 @@ class Rules:
     self.rules = definition.get('rules')
     if definition.get('randomize') != None:
       if self.rules != None: raise ValueError('Passed both randomize integer and preset rules.')
-      rule_amount = range(int(definition['randomize']))
-    elif self.rules = None:
-      rule_amount = range(random.randint(2,5))
-    
-    if self.rules = None:
+      rule_amount = int(definition['randomize'])
+    else:
+      rule_amount = random.randint(2,5)
+
+    if self.rules == None:
+      self.rules = []
       class_init = {}
       class_init['rule_amount'] = rule_amount 
-      for x in rule_amount:
+      for x in range(rule_amount):
         self.rules.append(Rule(class_init))
-
 
 class Grid:
   def __init__(self,definition={}):
     self.definition = definition
-    self.grid = definition.get('state')
+    self.state = definition.get('state')
     if self.state == None:
+      self.state = []
       self.x = definition.get('x',random.randint(5,10))
       self.y = definition.get('y',random.randint(5,10))
+      for x in range(self.x):
+        self.state.append([])
+        for y in range(self.y):
+          self.state[x].append(0)
     else:
-      pass
-      # Set x and y from loaded grid
-      # self.x =
-      # self.y =
+      self.x = len(self.state)
+      self.y = len(self.state[0])
 
 class Game:
   def __init__(self,rules=Rules(),grid=Grid()):
-    pass
+    self.rules = rules
+    self.grid = grid
+
+  def show(self):
+    for x in self.grid.state:
+      print(x)
--- test.py
+++ test.py
@@ -1,7 +1,41 @@
 import unittest
 import gol
 
-class TestRuleCreation(unittest.TestCase):
+class TestRandomGameCreation(unittest.TestCase):
 
   def test_randomization(self):
-    print('hi')
+    g = gol.Game()
+    self.assertEqual(g.grid.state[4][4],0)
+
+class TestGridCreation(unittest.TestCase):
+
+  def test_random_size(self):
+    self.g = gol.Grid()
+    self.assertTrue(self.g.x > 4)
+    self.assertTrue(self.g.x < 11)
+    self.assertTrue(self.g.y > 4)
+    self.assertTrue(self.g.y < 11)
+  
+  def test_random_state(self):
+    self.g = gol.Grid()
+    self.assertEqual(self.g.state[4][4],0)
+
+  def test_set_state(self):
+    state = [[0,0,0],[0,0,0]]
+    self.g = gol.Grid({'state': state})
+    self.assertEqual(self.g.x,2)
+    self.assertEqual(self.g.y,3)
+    self.assertEqual(self.g.state,state)
+  
+  def test_set_x_y(self):
+    self.g = gol.Grid({'x': 2, 'y': 1})
+    self.assertEqual(self.g.state,[[0],[0]])
+
+class TestRuleCreation(unittest.TestCase):
+
+  def test_random_alive(self):
+    self.g = gol.Rule({'rule_amount': 2})
+    self.assertTrue(self.g.alive in [True, False])
+
+if __name__ == '__main__':
+    unittest.main()
