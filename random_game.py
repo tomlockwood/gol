f694deeda8cf65dbeb92edf4c0ca502848db4d58
Tom Lockwood <tom.lockwood@bugcrowd.com>
2019-01-05 13:38:59 +1100

    Changing random state

--- random_game.py
+++ random_game.py
@@ -8,15 +8,15 @@ for y in range(1000000):
 
   grid = gol.Grid({'x': 50, 'y': 50})
 
-  g = gol.Game(rules=rules,grid=grid,period_retention=10)
+  g = gol.Game(rules=rules,grid=grid,period_retention=50)
 
   g.random_grid()
 
-  for x in range(300):
+  for x in range(5000):
     g.tick()
     if g.periodic != None:
       if g.ticks > 10:
-        g.seed.output('games/game_ticks{}_period{}_time{}.json'.format(
+        g.seed.output('games/ticks{}_period{}_time{}.json'.format(
           g.ticks,g.periodic,datetime.now().isoformat()))
       break
 
