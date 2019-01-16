f4b67fb484147dca7abbdd7077ec30bee5b4d142
Tom Lockwood <tom.lockwood@bugcrowd.com>
2019-01-16 18:11:31 +1100

    Tick text

--- lib/renderer.py
+++ lib/renderer.py
@@ -50,7 +50,7 @@ class Render:
             return 'Move'
       self.screen.fill((0,0,0))
       out = []
-      out.append(str(self.game.ticks))
+      out.append('Tick:   ' + str(self.game.ticks))
       self.generate_grid()
       for k in self.game.metadata:
         out.append('{}:   {}'.format(k,self.game.metadata[k]))
